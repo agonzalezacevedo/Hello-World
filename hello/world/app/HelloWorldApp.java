616bb62471831216eb8e89f5a4e1ae8a4c09ae65
Alexandre Johannes González Acevedo <agonzalezacevedo@nautilus10.danielcastelao.org>
2019-10-04 14:07:09 +0200

    Inicio

---
+++ hello/world/app/HelloWorldApp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world.app;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
